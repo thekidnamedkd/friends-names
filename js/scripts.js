adc0b234ee70cf0f351fa507d1601e169d6009c7
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-08 13:31:51 -0700

    write JavaScript for two practice prompts.
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
 event.preventDefault();
+friendsnames = ["Bob", "Rob", "Nob"]
+friendsnames.forEach(function(name) {
+  alert('My friends name is ' + name + '.')
+})
 })
